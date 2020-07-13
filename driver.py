4e1708fcecaa98997d634f6fd6b1711f855e216f
Muhamad Rafi Pamungkas <56276758+muhamadrafipamungkas@users.noreply.github.com>
2020-07-13 20:18:38 +0700

    Perbaikan print error

--- driver.py
+++ driver.py
@@ -3,7 +3,7 @@ from scrapper import ScrapperShopee
 
 inp = sys.argv
 if(len(inp) == 1):
-    print("Harap sertakan link produk shopee")
+    print("Harap sertakan link produk Shopee")
 else:
     coba = ScrapperShopee(str(inp[1]))
     coba.getPrice()
