e879b56d90fb4cb0478827c7a61306ef7b69cdae
Muhamad Rafi Pamungkas <56276758+muhamadrafipamungkas@users.noreply.github.com>
2020-07-14 15:07:14 +0700

    Update
    - Input pakai file txt

--- driver.py
+++ driver.py
@@ -1,15 +1,20 @@
+import csv
 from scrapper import ScrapperShopee
 
+
 if __name__ == "__main__":
+
     filepath = 'link.txt'
     with open(filepath) as fp:
+
         line = fp.readline()
         counter = 1
+
         while line:
+
             try:
-                print(line)
                 coba = ScrapperShopee(line, counter)
-                coba.getPrice()
+                coba.scrappe()
                 line = fp.readline()
                 counter += 1
             except Exception as err:
--- scrapper.py
+++ scrapper.py
@@ -5,7 +5,6 @@ class ScrapperShopee:
     url = None
     price = None
     line = 0
-
     
     def __init__(self, url, line):
         urllist = url.split(".")
@@ -17,23 +16,53 @@ class ScrapperShopee:
                 self.line = line
                 itemid = urllist[-1]
                 shopid = urllist[-2]
-                self.url = "https://shopee.co.id/api/v2/item/get?itemid="+itemid+"&shopid="+shopid
+                self.url = ("https://shopee.co.id/api/v2/item/get?itemid={}&shopid={}").format(itemid,shopid)
         else:
-            raise Exception("Link pada baris ke-"+self.line+" tidak valid ! ! !")
+            raise Exception(("Link pada baris ke-{} tidak valid ! ! !").format(line))
 
 
-    def getPrice(self):
+    def scrappe(self):
         try:
             s = requests.Session()
             page = s.get(self.url)
             jsontext = json.loads(page.text)
-            harga_min = str(jsontext['item']['price'])
-            harga_max = str(jsontext['item']['price_max'])
+            
+
+            # Variable produk
+            nama_produk = jsontext['item']['name']
+            rating = str(jsontext['item']['item_rating']['rating_star'])
+            rating = rating[:2]
+            review = str(jsontext['item']['item_rating']['rcount_with_context'])
+            harga_min = str(jsontext['item']['price']) # Perlu diganti template literal, kalau mau outputnya number
+            harga_max = str(jsontext['item']['price_max']) # Perlu diganti template literal, kalau mau outputnya number
             harga_min = harga_min[:-5]
             harga_max = harga_max[:-5]
+            deskripsi = str(jsontext['item']['description'])
+            stock = str(jsontext['item']['normal_stock'])
+            pengiriman = str(jsontext['item']['shop_location'])
+            link_gambar = []
+
+            for i in jsontext['item']['images']:
+                link_gambar.append(("https://cf.shopee.co.id/file/{}").format(i))
+            
+            print("==========================")
+            print(("Nama Produk: {}").format(nama_produk))
+            print(("Rating Produk: {} , dari {} review ").format(rating, review))
+
             if(harga_max == harga_min):
-                print("harga_produk: Rp "+harga_min)
+                print(("Harga Produk: Rp {}").format(harga_min))
             else:
-                print("Harga produk berkisar dari Rp "+harga_min+" sampai Rp "+harga_max)
+                print(("Harga Produk: dari Rp {} sampai Rp {}").format(harga_min, harga_max))
+
+            print(("Deskripsi: {}").format(deskripsi))
+
+            print(("Sisa Stock: {}").format(stock))
+
+            print(("Dikirim dari: {}").format(pengiriman))
+
+            print(("Link Gambar : {}").format(link_gambar))            
+
+                
+            print("==========================")
         except:
             raise Exception(("Produk pada baris ke-{} tidak tersedia ! ! !").format(self.line))
